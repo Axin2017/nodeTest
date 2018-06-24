0cdcb165bf1af488ecc1006c311e8e81b873d884
XinTan <838524739@qq.com>
2018-06-24 15:24:10 +0800

    http模块

--- simple_server.js
+++ simple_server.js
@@ -1,9 +1,22 @@
-let http=require('http');
+const http=require('http');
+const url=require('url');
 let handle=(req,res)=>{
-    console.log('request get');
-    res.writeHead(200, {'Content-Type': 'text/plain'});  
-    res.end(JSON.stringify({name:"谭新"}));
+    if(req.method=='GET'){
+        handle_get(req,res);
+    }else{
+        handle_post(req,res);
+    }
 }
-let server=http.createServer(handle);
+let handle_get=(req,res)=>{
+    let urlObj=url.parse(req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});  
+    res.end(JSON.stringify(urlObj));
+}
+let handle_post=(req,res)=>{
+    let urlObj=url.parse(req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});  
+    res.end(JSON.stringify(urlObj));
+}
+const server=http.createServer(handle);
 server.listen(8081);
 console.log('start listen 8081');
