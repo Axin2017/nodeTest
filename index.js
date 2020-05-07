92081f83ba5fe1025137e9093224d5be3c2ffe72
XinTan <838524739@qq.com>
2020-05-07 18:31:28 +0800

    添加svn-committer测试

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-const fs = require('fs')
 const path = require('path')
-const cmd = require('node-cmd')
-const ora = require('ora')
-const inquirer = require('inquirer')
-const http = require('http')
+const cwd = require('process').cwd()
 
-var request = require("request")
-for (let i = 1; i < 78; i++) {
-  const src = `https://book.yunzhan365.com/mlpe/smrl/files/mobile/${i}.jpg?x-oss-process=image/format,png`
-  const writeStream = fs.createWriteStream(`D:\\语文一年级下册\\第${i}页.png`)
-  const readStream = request(src)
-  readStream.pipe(writeStream)
-  readStream.on('end', function () {
-    console.log(`第${i}页下载成功`)
-  })
-  readStream.on('error', function () {
-    console.log(`第${i}页错误信息:` + err)
-  })
-  writeStream.on("finish", function () {
-    console.log(`第${i}页写入成功`);
-    writeStream.end();
-  })
-}
 
+const p = path.resolve(process.cwd(),'svncommitter.config.js')
+const a = require(p)
+console.log(a)
